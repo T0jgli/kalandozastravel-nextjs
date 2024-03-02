3692c31df6f02ddc8a610ac99359ab9e27073416
T0jgli <kovalikadam00@gmail.com>
2024-03-02 20:34:19 +0100

    dont need yet

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = withBundleAnalyzer({
     experimental: {
         scrollRestoration: true,
     },
-    assetPrefix: process.env.NODE_ENV === "production" ? "https://cdn.kalandozas.hu" : undefined,
+    // assetPrefix: process.env.NODE_ENV === "production" ? "https://cdn.kalandozas.hu" : undefined,
     async headers() {
         return [
             {
