6d87f4d6ee646492340a2bb3f7afe641b41357b7
Kovalik <adam.kovalik@evosoft.com>
2024-06-26 10:14:27 +0200

    switch to client

--- components/HomeComponents/HomeBody.js
+++ components/HomeComponents/HomeBody.js
@@ -5,6 +5,7 @@ import InfoSections from "./InfoSections";
 import Streak from "./Streak";
 import Cards from "./Cards";
 import Loading from "../GlobalComponents/Loading";
+import { sortAndGroupData } from "../../lib/helpers/travels";
 
 const Newsletter = dynamic(() => import("./Newsletter"), {
     loading: () => <Loading />,
@@ -20,7 +21,7 @@ const HomeBody = ({ questionsAnswers, travels }) => {
     return (
         <>
             <Newsletter />
-            <Cards travels={travels} />
+            <Cards travels={sortAndGroupData(travels)} />
             <Gallery />
             <Streak />
             <InfoSections />
